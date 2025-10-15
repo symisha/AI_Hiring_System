825b25885c06b9ea7907d7e2950b8c2157f4c2c2
symisha <symishaimam@gmail.com>
2025-10-15 21:40:45 +0500

    Text from resume extracted

---
+++ backend/app/services/shortlisting_resume.py
@@ -0,0 +1,4 @@
+#extract all the rating from database
+#check the deviation of the ratings
+#that decides the threshold for shortlisting
+# if the rating is above the threshold, then the resume is shortlisted
