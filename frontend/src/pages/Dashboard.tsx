e83a23bfa32d26b3c423c25256259cd9fc88da25
symisha <symishaimam@gmail.com>
2025-11-10 21:11:03 +0500

    Variable localhost

--- frontend/src/pages/Dashboard.tsx
+++ frontend/src/pages/Dashboard.tsx
@@ -32,7 +32,7 @@ const Dashboard = () => {
         return;
       }
 
-      const response = await fetch("http://localhost:8000/routes/dashboard-info", {
+      const response = await fetch(import.meta.env.VITE_APP_NAME + "/routes/dashboard-info", {
         headers: {
           Authorization: `Bearer ${token}`,
           "Content-Type": "application/json",
