cf8451a967d9ff1b77ad353c50ddf2aacd8ec40a
symisha <symishaimam@gmail.com>
2026-05-11 22:34:33 +0500

    third terminal

--- backend/runworker/run_worker.py
+++ backend/runworker/run_worker.py
@@ -1,5 +1,5 @@
 import asyncio
-from app.services.screening_scheduler import screening_scheduler_loop
+from backend.app.services.screening_scheduler import screening_scheduler_loop
 
 if __name__ == "__main__":
     print("[WORKER] AI Screening Engine Starting...")
